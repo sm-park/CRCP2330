17cec4f30eb9892214495eebb46ca6c32322e2a2
sm-park <samp@smu.edu>
2018-10-15 14:42:05 -0500

    Mult.hack completed.

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// COUNT = RAM[1]
+// IF COUNT > 0
+// 	RAM[2] = RAM[2] + RAM[0]
+// 	COUNT = COUNT+1
+
+	@COUNT
+	M = 1
+
+	@R2
+	M = 0
+
+	@R0
+	D = M
+
+	@HALT
+	D;JEQ
+
+	@R1
+	D = M
+
+	@HALT
+	D;JEQ
+
+(LOOP)
+	
+	@R0
+	D = M
+
+	@R2
+	M = M + D
+
+	@COUNT
+	D = M + 1
+	M = D
+
+	@R1
+	D = D - M
+	@LOOP
+	D;JLE
+
+	@HALT
+(HALT)
+	0;JMP
